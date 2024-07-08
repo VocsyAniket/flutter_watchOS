eb06e84c19606e7f6a012fdd2ab1f4b9ac033018
Aniket <vocsyaniket@gmail.com>
2024-07-08 14:15:53 +0530

    init

---
+++ android/app/src/main/java/com/watchos/watchos_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.watchos.watchos_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/watchos Watch App/watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchosApp.swift
+//  watchos Watch App
+//
+//  Created by DREAMWORLD on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
